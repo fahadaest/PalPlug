50a24dbc2ce4c87c11cd013f167edd78e23ba9df
ali rehan <ali.rehan2842@gmail.com>
2024-09-10 19:20:27 +0500

    Solved Imports Issue

--- src/app/utils/firebase.js
+++ src/app/utils/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth} from 'firebase/auth';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
