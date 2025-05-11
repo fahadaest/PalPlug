2b7000d63cbb7e6acd565c36b9a7e90b846f550d
Fahad Nasir <fahadaestt@gmail.com>
2025-05-12 00:06:23 +0400

    frontend phase 11 completewd

--- src/app/utils/firebase.js
+++ src/app/utils/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth} from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -10,4 +10,4 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-export { auth};
+export { auth };
